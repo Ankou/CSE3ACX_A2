9bdebbf9502453c190b4eae9044d901932dbdde7
Ankou <rickyjfield@hotmail.com>
2024-02-22 21:28:38 +1100

    Shell scripts testing

--- cleanup.sh
+++ cleanup.sh
@@ -3,8 +3,10 @@
 # Shell script to clean up resources
 resources=~/resources.json
 
-VPC=$(jq -r '."VPC-ID"' $resources )
+VPC=$( jq -r '."VPC-ID"' $resources )
+subnet0=$( jq -r '."Subnet0"' $resources )
 
+aws ec2 delete-subnet --subnet-id $subnet0
 aws ec2 delete-vpc --vpc-id $VPC
 
 rm -f $resources
